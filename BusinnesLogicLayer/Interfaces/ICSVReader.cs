2c081ea3c6dd4b64ec3c2af5492ff1aa26bc82e3
SerhiiYakubyshyn <70217790+SerhiiYakubyshyn@users.noreply.github.com>
2021-06-22 10:30:00 +0300

    Add files via upload

---
+++ BusinnesLogicLayer/Interfaces/ICSVReader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLogicLayer.Interfaces
+{
+    public interface ICSVReader<T>
+    {
+        IEnumerable<T> Parser(Stream stream);
+    }
+}
---
+++ BusinnesLogicLayer/Interfaces/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinnesLogicLayer.Interfaces
+{
+    public interface IService<T>
+    {
+        IEnumerable<T> GetListDTO();
+        T FindDTO(int? id);
+        void CreateDTO(T item);
+        void UpdateDTO(T item);
+        void DeleteDTO(int? id);
+        void AddListPeoples(IEnumerable<T> item);
+    }
+}
